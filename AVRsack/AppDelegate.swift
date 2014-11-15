b600a76528d9c3500f7b7bfd27e3ff2e3d0d624c
Matthias Neeracher <microtherion@gmail.com>
2014-11-15 04:39:10 +0100

    Initial Commit

---
+++ AVRsack/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AVRsack
+//
+//  Created by Matthias Neeracher on 11/15/14.
+//  Copyright (c) 2014 Aere Perennius. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
